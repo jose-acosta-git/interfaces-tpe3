fe8c4160676cf9c7900c92046a715e18bb7a9d47
jose-acosta-git <jose.acosta.git@gmail.com>
2023-05-01 17:28:26 -0300

    add clean, reset and save feature

--- js/image.js
+++ js/image.js
@@ -1,9 +1,13 @@
+/** @type {HTMLCanvasElement} */
 "use strict";
 
-const btn = document.getElementById('bg-image');
+const upload = document.getElementById('bg-image');
+const reset = document.getElementById('reset');
+const clean = document.getElementById('clean');
+const save = document.getElementById('save');
 const img = new Image();
 
-btn.addEventListener('change', (e) => {
+upload.addEventListener('change', (e) => {
     if (e.target.files) {
         let file = e.target.files[0];
         
@@ -14,8 +18,27 @@ btn.addEventListener('change', (e) => {
             img.src = e.target.result;
         }
 
-        img.onload = () => {
+        img.onload = () => {    
+            context.clearRect(0, 0, canvas.width, canvas.height);
             context.drawImage(img, 0, 0, canvas.width, canvas.height);
         }
     }
+})
+
+reset.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (img.src) {
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+})
+
+clean.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+})
+
+save.addEventListener('click', () => {
+    let link = document.createElement('a');
+    link.download = 'canvas.png';
+    link.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+    link.click();
 })
